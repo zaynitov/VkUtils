747449e6e7c35766ad93acb8e34265b8db8114c7
Your Name <you@example.com>
2020-04-25 20:12:41 +0500

    fileUTil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) throws ClientException, ApiException, IOException, URISyntaxException {
         VkUtils.downloadPhotos();
        // FileUtil.findInContent();
+        System.out.println("ASD");
     }
 }
